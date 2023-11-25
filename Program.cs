be4e5616e0986aee4f1ed43e46e72244de2598d4
Dakota Johnson <47164434+D7K7@users.noreply.github.com>
2023-11-25 02:48:00 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ public class Kata
 {
   public static int[] ArrayDiff(int[] a, int[] b)
   {
+    //HashSets work for bigger arrays, in a realistic setting.
     var setB = new HashSet<int>(b);
     return a.Where(x => !setB.Contains(x)).ToArray();
   }
